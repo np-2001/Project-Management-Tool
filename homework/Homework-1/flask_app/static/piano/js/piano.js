0150a7a37321ed8efead88341bb21bcb0dc604ea
np-2001 <polavarapunitin@gmail.com>
2024-02-08 13:07:42 -0500

    Trying to fix hover

--- homework/Homework-1/flask_app/static/piano/js/piano.js
+++ homework/Homework-1/flask_app/static/piano/js/piano.js
@@ -1,22 +1,28 @@
+
+// const white = document.querySelectorAll(".white-key")
+// const black = document.querySelectorAll(".black-key")
+
+
+// for (const key of white) {
+//     key.addEventListener('mouseover',DisplayLetters);
+//     key.addEventListener('mouseout',HideLetters);
+// }
+
+// for (const key of black) {
+//     key.addEventListener('mouseover',DisplayLetters);
+//     key.addEventListener('mouseout',HideLetters);
+// }
+
+
 const key_letters = document.querySelectorAll(".letter");
 
-const white = document.querySelectorAll(".white-key")
-const black = document.querySelectorAll(".black-key")
+const piano = document.querySelector(".keys")
 
-
-for (const key of white) {
-    key.addEventListener('mouseover',DisplayLetters);
-    key.addEventListener('mouseout',HideLetters);
-}
-
-for (const key of black) {
-    key.addEventListener('mouseover',DisplayLetters);
-    key.addEventListener('mouseout',HideLetters);
-}
+piano.addEventListener('mouseover',DisplayLetters);
+piano.addEventListener('mouseout',HideLetters);
 
 function DisplayLetters() {
     for (const letter of key_letters) {
-
         letter.style.opacity = '100%';
     }
     
